65d94ab7a758a3fb5bff5a1b5ca42368d100928e
Jcarlos545 <jaocarlos.545@gmail.com>
2019-03-28 10:39:14 -0300

    Acrescentada mensagem de bom dia

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -5,7 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Olá Mundo!");
+		System.out.println("Bom dia!");
 
 	}
 
